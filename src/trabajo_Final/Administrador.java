b3a51cec94513b7660714a7ad55da64dea471c85
pablocasta1980 <pcastanog@uqvirtual.edu.co>
2023-05-22 12:09:03 -0500

    primero

---
+++ src/trabajo_Final/Administrador.java
@@ -0,0 +1,34 @@
+package trabajo_Final;
+
+import java.util.Date;
+
+public class Administrador extends Persona {
+    // Constructor
+    public Administrador(String nombre, String apellido, int id, String correo, String contrasena) {
+        super(nombre, apellido, id, correo, contrasena);
+    }
+
+    // Funcionalidades adicionales del administrador
+
+    public void registrarEmpleado(Empleado empleado) {
+        // Lógica para registrar un nuevo empleado en el sistema
+    }
+
+    public void actualizarEmpleado(Empleado empleado) {
+        // Lógica para gestionar los datos de un empleado existente (actualizar datos)
+    }
+    
+    public void bloquearEmpleado(Empleado empleado) {
+        // Lógica para gestionar los datos de un empleado existente (bloquear empleado)
+    }
+    
+    public void eliminarEmpleado(Empleado empleado) {
+        // Lógica para gestionar los datos de un empleado (eliminarlo base de datos)
+    }
+    
+    public void generarReportes( Date fechaInicio, Date fechaFinal) {
+        // Lógica para ver los reportes de negocios realizados por los empleados
+    }
+
+}
+
